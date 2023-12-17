cac6904892f864d7cfc4bfade18317d927db3757
Juliano Silva <juliano.silva@recife.pe.gov.br>
2023-12-17 09:44:50 -0300

    Project skeleton

---
+++ GlottoApp/Glotto/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Glotto.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
